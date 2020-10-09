c9118693227be3cefdef84194c5994ba5fa7b708
Anthony <anthonyco@hotmail.com>
2020-10-08 22:16:44 -0400

    Added webpack server, added slider feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import './styles.css'
+
 function hello() {
-    console.log("Hello world")
+    let div = document.createElement("div")
+    div.id = "hi"
+    div.innerHTML = "<p>Hello there</p>"
+    document.body.appendChild(div)
 }
+
+function onSliderClick() {
+    let slider = document.getElementById('slider')
+
+    if (!slider.className || slider.className == 'ltr') {
+        slider.className = 'rtl'
+    } else {
+        slider.className = 'ltr'
+    }
+}
+
+document.getElementById("slider-button").addEventListener('click', onSliderClick)
